83a1eccecc378ac6c2dc325fd50e75b754d37246
Jeremy Magland <jeremy.magland@gmail.com>
2016-03-30 19:10:55 -0400

    mdaclient

---
+++ mountainsort/src/mda/mdaclient.cpp
@@ -0,0 +1,8 @@
+/******************************************************
+** See the accompanying README and LICENSE files
+** Author(s): Jeremy Magland
+** Created: 3/30/2016
+*******************************************************/
+
+
+
---
+++ mountainsort/src/mda/mdaclient.h
@@ -0,0 +1,12 @@
+/******************************************************
+** See the accompanying README and LICENSE files
+** Author(s): Jeremy Magland
+** Created: 3/30/2016
+*******************************************************/
+
+
+#ifndef MDACLIENT_H
+#define MDACLIENT_H
+
+#endif // MDACLIENT_H
+
