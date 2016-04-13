ffda9bee1deb3fe3b3b34f6de25cad8e5a2fcdc6
Jeremy Magland <magland@jm02.scdanet.org>
2016-04-13 10:58:40 -0400

    add merge labels

---
+++ mountainsort/src/processors/merge_labels_processor.h
@@ -0,0 +1,11 @@
+/******************************************************
+** See the accompanying README and LICENSE files
+** Author(s): Jeremy Magland
+** Created: 4/13/2016
+*******************************************************/
+
+#ifndef MERGE_LABELS_PROCESSOR_H
+#define MERGE_LABELS_PROCESSOR_H
+
+#endif // MERGE_LABELS_PROCESSOR_H
+
