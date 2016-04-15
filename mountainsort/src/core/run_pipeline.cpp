ba148afcb12643b667c5cc1b8dc49cd30bb70995
Jeremy Magland <magland@jm02.scdanet.org>
2016-04-15 08:48:03 -0400

    implementing pipeline processing

---
+++ mountainsort/src/core/run_pipeline.cpp
@@ -0,0 +1,7 @@
+/******************************************************
+** See the accompanying README and LICENSE files
+** Author(s): Jeremy Magland
+** Created: 4/15/2016
+*******************************************************/
+
+
---
+++ mountainsort/src/core/run_pipeline.h
@@ -0,0 +1,11 @@
+/******************************************************
+** See the accompanying README and LICENSE files
+** Author(s): Jeremy Magland
+** Created: 4/15/2016
+*******************************************************/
+
+#ifndef RUN_PIPELINE_H
+#define RUN_PIPELINE_H
+
+#endif // RUN_PIPELINE_H
+
