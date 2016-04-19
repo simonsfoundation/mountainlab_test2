8c7a9172577b8abf64527f37ac7b429748008c29
Jeremy Magland <jeremy.magland@gmail.com>
2016-04-19 18:02:22 -0400

    impelement confusion matrix processor

---
+++ mountainsort/src/processors/confusion_matrix.h
@@ -0,0 +1,6 @@
+#ifndef CONFUSION_MATRIX_H
+#define CONFUSION_MATRIX_H
+
+bool confusion_matrix(const char *firings1_path,const char *firings2_path,const char *output_path,int max_matching_offset);
+
+#endif // CONFUSION_MATRIX_H
---
+++ mountainsort/src/processors/confusion_matrix_processor.cpp
@@ -0,0 +1,44 @@
+/******************************************************
+** See the accompanying README and LICENSE files
+** Author(s): Jeremy Magland
+** Created: 4/19/2016
+*******************************************************/
+
+#include "confusion_matrix_processor.h"
+#include "confusion_matrix.h"
+
+class confusion_matrix_ProcessorPrivate
+{
+public:
+    confusion_matrix_Processor *q;
+};
+
+confusion_matrix_Processor::confusion_matrix_Processor() {
+    d=new confusion_matrix_ProcessorPrivate;
+    d->q=this;
+
+    this->setName("confusion_matrix");
+    this->setVersion("0.1");
+    this->setInputFileParameters("firings1","firings2");
+    this->setOutputFileParameters("output");
+    this->setRequiredParameters("max_matching_offset");
+}
+
+confusion_matrix_Processor::~confusion_matrix_Processor() {
+    delete d;
+}
+
+bool confusion_matrix_Processor::check(const QMap<QString, QVariant> &params)
+{
+    if (!this->checkParameters(params)) return false;
+    return true;
+}
+
+bool confusion_matrix_Processor::run(const QMap<QString, QVariant> &params)
+{
+    QString firings1_path=params["firings1"].toString();
+    QString firings2_path=params["firings2"].toString();
+    QString output_path=params["output"].toString();
+    int max_matching_offset=params["max_matching_offset"].toInt();
+    return confusion_matrix(firings1_path.toLatin1().data(),firings2_path.toLatin1().data(),output_path.toLatin1().data(),max_matching_offset);
+}
