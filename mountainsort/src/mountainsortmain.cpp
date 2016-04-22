6318c6da9f7f702cac03b3136466d901dca0cfb4
Jeremy Magland <magland@jm02.scdanet.org>
2016-04-22 13:43:21 -0400

    misc

--- mountainsort/src/mountainsortmain.cpp
+++ mountainsort/src/mountainsortmain.cpp
@@ -135,9 +135,9 @@ bool run_process(MSProcessManager* PM, QJsonObject process)
 
 void display_error(QJSValue result)
 {
-    qDebug() << result.property("name").toString();
-    qDebug() << result.property("message").toString();
-    qDebug() << QString("%1 line %2").arg(result.property("fileName").toString()).arg(result.property("lineNumber").toInt());
+    qDebug()  << result.property("name").toString();
+    qDebug()  << result.property("message").toString();
+    qDebug()  << QString("%1 line %2").arg(result.property("fileName").toString()).arg(result.property("lineNumber").toInt());
 }
 
 int run_script(const QStringList& script_fnames)
