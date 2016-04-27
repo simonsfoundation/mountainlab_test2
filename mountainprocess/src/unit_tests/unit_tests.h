269d1b058d3cffc279f7956b6df1d1c337a3ea66
Jeremy Magland <jeremy.magland@gmail.com>
2016-04-27 08:48:35 -0400

    add

---
+++ mountainprocess/src/unit_tests/unit_tests.h
@@ -0,0 +1,14 @@
+/******************************************************
+** See the accompanying README and LICENSE files
+** Author(s): Jeremy Magland
+*******************************************************/
+
+#ifndef UNIT_TESTS_H
+#define UNIT_TESTS_H
+
+#include <QString>
+
+void run_unit_test(const QString &test_name);
+void run_all_unit_tests();
+
+#endif // UNIT_TESTS_H
