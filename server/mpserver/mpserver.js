e86a5fbfaeb3d39da053b03f2e89902367a0bf8e
Jeremy Magland <magland@jm02.scdanet.org>
2016-05-09 06:29:14 -0400

    clean up

--- server/mpserver/mpserver.js
+++ server/mpserver/mpserver.js
@@ -56,9 +56,8 @@ http.createServer(function (REQ, RESP) {
 		var headers = {};
 		
 		//allow cross-domain requests
+		/// TODO refine this
 		
-		// IE8 does not allow domains to be specified, just the *
-		// headers["Access-Control-Allow-Origin"] = req.headers.origin;
 		headers["Access-Control-Allow-Origin"] = "*";
 		headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
 		headers["Access-Control-Allow-Credentials"] = false;
@@ -68,18 +67,7 @@ http.createServer(function (REQ, RESP) {
 		RESP.end();
 	}
 	else if(REQ.method=='GET') {
-		send_json_response({success:false,error:'mpserver'});
-		/*
-		if (url_parts.pathname=='/ohcommentserver/getAllComments') {
-			var page_id=url_parts.query.page_id||'';
-            get_all_comments(page_id,function(resp) {
-				send_json_response(resp);
-			});
-		}
-		else {
-			send_json_response({success:false,error:'Unrecognized url path.'});
-		}
-		*/
+		send_json_response({success:false,error:'mpserver does not support GET requests'});
 	}
 	else if(REQ.method=='POST') {
 		receive_json_post(REQ,function(req) {
@@ -91,65 +79,7 @@ http.createServer(function (REQ, RESP) {
 			X.handleRequest(req,function(resp) {
 				send_json_response(resp);	
 			});
-
 		});
-        
-		//upload a file!
-		
-        /*
-		if (url_parts.pathname=='/wisdmfileserver/setFileData') {
-			var fsname=url_parts.query.fsname||'';
-			var path=url_parts.query.path||'';
-			var text=url_parts.query.text||'';
-			
-			if (!create_path_for_file(path,wisdmconfig.wisdmfileserver.data_path+'/files/'+fsname)) {
-				send_json({success:false,error:'Unable to create folder for file.'});
-				return;
-			}
-			
-			var path0=wisdmconfig.wisdmfileserver.data_path+'/files/'+fsname+'/'+path;
-			//write to a temporary file (later we'll move it over)
-			var tmppath0=path0+'.'+make_random_id(4)+'.tmp';
-			
-			var file_size=REQ.headers['content-length'];
-			if (file_size>10*1000*1000) {
-				send_json_response({success:false,error:'File is too large: '+file_size});
-				return;
-			}
-			
-			var out_stream=fs.createWriteStream(tmppath0);
-			
-			var byte_count=0;
-			
-			var done=false;
-			REQ.on('data',function(d) {
-				if (done) return;
-				out_stream.write(d);
-				byte_count+=d.length;
-			});
-			REQ.on('end',function() {
-				if (done) return;
-				if (byte_count!=file_size) {
-					send_json_response({success:false,error:'Unexpected file size: '+byte_count+' <> '+file_size});
-					done=true;
-					return;
-				}
-				if (file_exists(path0)) {
-					remove_file(path0);
-				}
-				if (!rename_file(tmppath0,path0)) {
-					send_json_response({success:false,error:'Problem renaming file'});
-					done=true;
-					return;
-				}
-				send_json_response({success:true});
-				done=true;
-			});
-		}
-		else {
-			send_json_response({success:false,error:'Unexpected path for POST: '+url_parts.pathname});
-		}
-        */
 	}
 
 	function receive_json_post(REQ,callback) {
