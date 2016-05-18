7e03e88e61c822790e84101730a920df2b8870d6
Jeremy Magland <magland@jm02.scdanet.org>
2016-05-17 22:00:10 -0400

    copy taskprogressview.cpp from ML-72 -- has improvements from Witold

--- mountainview/src/taskprogressview.cpp
+++ mountainview/src/taskprogressview.cpp
@@ -1,6 +1,6 @@
 /******************************************************
 ** See the accompanying README and LICENSE files
-** Author(s): Jeremy Magland and Witold Wysota
+** Author(s): Jeremy Magland
 ** Created: 4/30/2016
 *******************************************************/
 
@@ -19,80 +19,77 @@
 #include <QShortcut>
 #include <QApplication>
 #include <QClipboard>
-
-/// Witold - please no line wrap in popup log editor
-/// Witold - fyi I changed the date format on the log so I can see the seconds/milliseconds. Useful for seeing what takes the most time.
-/// Witold - for long tasks, the duration only updates when mouse moves (odd behavior) perhaps should update every second for ongoing tasks.
+#include <QDesktopWidget>
 
 class TaskProgressViewDelegate : public QStyledItemDelegate {
 public:
-    TaskProgressViewDelegate(QObject *parent = 0) : QStyledItemDelegate(parent) {}
-    QSize sizeHint(const QStyleOptionViewItem &option, const QModelIndex &index) const {
+	TaskProgressViewDelegate(QObject *parent = 0) : QStyledItemDelegate(parent) {}
+	QSize sizeHint(const QStyleOptionViewItem &option, const QModelIndex &index) const {
         if (index.parent().isValid())
             return QStyledItemDelegate::sizeHint(option, index);
-        QSize sh = QStyledItemDelegate::sizeHint(option, index);
-        sh.setHeight(sh.height()*2);
-        return sh;
-    }
-    void paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const {
+		QSize sh = QStyledItemDelegate::sizeHint(option, index);
+		sh.setHeight(sh.height()*2);
+		return sh;
+	}
+	void paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const {
         if (index.internalId() != 0xDEADBEEF || index.column() != 0) {
             QStyledItemDelegate::paint(painter, option, index);
             return;
         }
-        QStyleOptionViewItem opt = option;
-        opt.text = "";
-        opt.displayAlignment = Qt::AlignTop|Qt::AlignLeft;
-        QStyledItemDelegate::paint(painter, opt, index);
-        qreal progress = index.data(Qt::UserRole).toDouble();
-        if (progress < 1.0) {
+		QStyleOptionViewItem opt = option;
+		opt.text = "";
+		opt.displayAlignment = Qt::AlignTop|Qt::AlignLeft;
+		QStyledItemDelegate::paint(painter, opt, index);
+		qreal progress = index.data(Qt::UserRole).toDouble();
+		if (progress < 1.0) {
             QPen p = painter->pen();
             QFont f = painter->font();
             p.setColor((option.state & QStyle::State_Selected) ? Qt::white : Qt::darkGray);
             f.setPointSize(f.pointSize()-3);
             QFontMetrics smallFm(f);
             int elapsedWidth = smallFm.width("MMMMM");
-            QStyleOptionProgressBar progOpt;
-            progOpt.initFrom(opt.widget);
-            progOpt.rect = option.rect;
-            progOpt.minimum = 0;
-            progOpt.maximum = 100;
-            progOpt.progress = progress*100;
-            progOpt.rect.setTop(progOpt.rect.center().y());
+			QStyleOptionProgressBar progOpt;
+			progOpt.initFrom(opt.widget);
+			progOpt.rect = option.rect;
+			progOpt.minimum = 0;
+			progOpt.maximum = 100;
+			progOpt.progress = progress*100;
+			progOpt.rect.setTop(progOpt.rect.center().y());
             progOpt.rect.adjust(4+elapsedWidth+4, 2, -4, -2);
-            if (option.widget) {
-                option.widget->style()->drawControl(QStyle::CE_ProgressBar, &progOpt, painter, option.widget);
-            }
-            painter->save();
-            painter->setPen(p);
-            painter->setFont(f);
-            QRect r = option.rect;
-            r.setTop(option.rect.center().y());
-            r.adjust(4, 2, -4, -2);
+			if (option.widget) {
+				option.widget->style()->drawControl(QStyle::CE_ProgressBar, &progOpt, painter, option.widget);
+			}
+			painter->save();
+			painter->setPen(p);
+			painter->setFont(f);
+			QRect r = option.rect;
+			r.setTop(option.rect.center().y());
+			r.adjust(4, 2, -4, -2);
             r.setRight(r.left()+elapsedWidth);
 
-            qreal duration = index.data(Qt::UserRole+1).toDateTime().msecsTo(QDateTime::currentDateTime()) / 1000.0;
+			qreal duration = index.data(Qt::UserRole+1).toDateTime().msecsTo(QDateTime::currentDateTime()) / 1000.0;
             if (duration < 100)
                 painter->drawText(r, Qt::AlignRight|Qt::AlignVCenter, QString("%1s").arg(duration, 0, 'f', 2));
             else
                 painter->drawText(r, Qt::AlignRight|Qt::AlignVCenter, QString("%1s").arg(qRound(duration)));
-            painter->restore();
-        } else {
-            painter->save();
-            QPen p = painter->pen();
-            QFont f = painter->font();
-            p.setColor((option.state & QStyle::State_Selected) ? Qt::white : Qt::darkGray);
-            f.setPointSize(f.pointSize()-2);
-            painter->setPen(p);
-            painter->setFont(f);
-            QRect r = option.rect;
-            r.setTop(option.rect.center().y());
-            r.adjust(4, 2, -4, -2);
+			painter->restore();
+		} else {
+			painter->save();
+			QPen p = painter->pen();
+			QFont f = painter->font();
+			p.setColor((option.state & QStyle::State_Selected) ? Qt::white : Qt::darkGray);
+			f.setPointSize(f.pointSize()-2);
+			painter->setPen(p);
+			painter->setFont(f);
+			QRect r = option.rect;
+			r.setTop(option.rect.center().y());
+			r.adjust(4, 2, -4, -2);
 
-            qreal duration = index.data(Qt::UserRole+1).toDateTime().msecsTo(index.data(Qt::UserRole+2).toDateTime()) / 1000.0;
-            painter->drawText(r, Qt::AlignLeft|Qt::AlignVCenter, QString("Completed in %1s").arg(duration));
-            painter->restore();
-        }
-    }
+			qreal duration = index.data(Qt::UserRole+1).toDateTime().msecsTo(index.data(Qt::UserRole+2).toDateTime()) / 1000.0;
+			painter->drawText(r, Qt::AlignLeft|Qt::AlignVCenter, QString("Completed in %1s").arg(duration));
+			painter->restore();
+		}
+	}
 };
 
 class TaskProgressModel : public QAbstractItemModel {
@@ -184,28 +181,12 @@ public:
         switch (role) {
         case Qt::EditRole:
         case Qt::DisplayRole:
-            // modified by jfm -- 5/17/2016
-            if (!task.error.isEmpty()) {
-                return task.label+": "+task.error;
-            }
-            else {
-                return task.label;
-            }
+            return task.label;
         case Qt::ToolTipRole:
-            // modified by jfm -- 5/17/2016
-            if (!task.description.isEmpty())
-                return task.description;
-            else
-                return taskData(index,Qt::DisplayRole);
+            return task.description;
         case Qt::ForegroundRole: {
-            // modified by jfm -- 5/17/2016
-            if (!task.error.isEmpty()) {
-                return QColor(Qt::red);
-            }
-            else {
-                if (task.progress < 1)
-                    return QColor(Qt::blue);
-            }
+            if (task.progress < 1)
+                return QColor(Qt::blue);
             return QVariant();
         }
         case ProgressRole:
@@ -240,8 +221,7 @@ protected:
         return entries.join("\n");
     }
     QString singleLog(const TaskProgressLogMessage &msg, const QString &prefix = QString()) const {
-        QString format="yyyy-MM-dd|hh:mm:ss.zzz"; //format by jfm
-        return QString("%1%2: %3").arg(prefix).arg(msg.time.toString(format)).arg(msg.message);
+        return QString("%1%2: %3").arg(prefix).arg(msg.time.toString(Qt::ISODate)).arg(msg.message);
     }
 
 private:
@@ -261,11 +241,12 @@ TaskProgressView::TaskProgressView()
     d = new TaskProgressViewPrivate;
     d->q = this;
     setSelectionMode(ContiguousSelection);
-    setItemDelegate(new TaskProgressViewDelegate(this));
+	setItemDelegate(new TaskProgressViewDelegate(this));
     TaskProgressModel *model = new TaskProgressModel(this);
     setModel(model);
     header()->setSectionResizeMode(0, QHeaderView::ResizeToContents);
     header()->hide();
+    setExpandsOnDoubleClick(false);
     connect(this, SIGNAL(doubleClicked(QModelIndex)), this, SLOT(showLogMessages(QModelIndex)));
     QShortcut *copyToClipboard = new QShortcut(QKeySequence(QKeySequence::Copy), this);
     connect(copyToClipboard, SIGNAL(activated()), this, SLOT(copySelectedToClipboard()));
@@ -315,6 +296,9 @@ void TaskProgressView::showLogMessages(const QModelIndex &index)
     QDialog dlg(this);
     dlg.setWindowTitle(tr("Log messages for %1").arg(index.data().toString()));
     QPlainTextEdit *te = new QPlainTextEdit;
+    QFont f;
+    f.setPointSize(f.pointSize()-2);
+    te->setFont(f);
     te->setReadOnly(true);
     QDialogButtonBox *bb = new QDialogButtonBox;
     QObject::connect(bb, SIGNAL(accepted()), &dlg, SLOT(accept()));
@@ -324,6 +308,10 @@ void TaskProgressView::showLogMessages(const QModelIndex &index)
     l->addWidget(te);
     l->addWidget(bb);
     te->setPlainText(index.data(TaskProgressModel::LogRole).toString());
+    QRect r = QApplication::desktop()->screenGeometry(&dlg);
+    r.setWidth(r.width()/2);
+    r.setHeight(r.height()/2);
+    dlg.resize(r.size());
     dlg.exec();
 }
 
