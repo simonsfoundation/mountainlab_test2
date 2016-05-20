156da2e19d5d285a09caa71be75e6d532e8f468d
ahbarnett <ahb@math.dartmouth.edu>
2016-05-20 12:09:46 -0400

    spikespy_simple_test removed Stats Toolbox dependency

--- spikespy/matlab/spikespy_simple_test.m
+++ spikespy/matlab/spikespy_simple_test.m
@@ -1,6 +1,10 @@
 function H=spikespy_simple_test(M,N,num_labels)
+% SPIKESPY_SIMPLE_TEST  call spikespy with random data as self test.
+%
+% H=spikespy_simple_test(M,N,num_labels), all inputs are optional
+% (if not given, defaults used)
 
-rng(1); %so we get the same array every time --> efficient loading in ssviewqt C++ program
+rng(1); %so we get the same array every time --> efficient loading
 
 if (nargin<1) M=6; end;
 if (nargin<2) N=1e6; end;
@@ -11,7 +15,7 @@ tt=linspace(-1,1,N);
 for ch=1:M
 	X(ch,:)=X(ch,:).*sin(tt*2*pi*10*ch^2);
 end;
-tj=randsample(size(X,2),num_labels)';
+tj=randi(size(X,2),[1 num_labels 1]);  % removed dependence on stats toolbox
 lj=ceil(rand(size(tj))*size(X,1));
 
 opts.sampling_freq=20000;
