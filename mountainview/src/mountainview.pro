6e34f1ba5caf53f43c02e8dcb5044f0c352b5eca
Jeremy Magland <jeremy.magland@gmail.com>
2016-06-13 14:34:54 -0400

    dev

--- mountainview/src/mountainview.pro
+++ mountainview/src/mountainview.pro
@@ -56,6 +56,7 @@ HEADERS += \
     mvviewagent.h \
     mvstatusbar.h \
     mvtimeseriesview.h \
+    mvtimeseriesview2.h \
     multiscaletimeseries.h \
     mvtimeseriesrendermanager.h \
     spikespywidget.h \
@@ -100,6 +101,7 @@ SOURCES += mountainviewmain.cpp \
     mvviewagent.cpp \
     mvstatusbar.cpp \
     mvtimeseriesview.cpp \
+    mvtimeseriesview2.cpp \
     multiscaletimeseries.cpp \
     mvtimeseriesrendermanager.cpp \
     spikespywidget.cpp \
---
+++ mountainview/src/mvtimeseriesview2.h
@@ -0,0 +1,64 @@
+/******************************************************
+** See the accompanying README and LICENSE files
+** Author(s): Jeremy Magland
+** Created: 5/27/2016
+*******************************************************/
+
+#ifndef MVTIMESERIESVIEW2_H
+#define MVTIMESERIESVIEW2_H
+
+#include <QWidget>
+#include <diskreadmda.h>
+#include "mvviewagent.h"
+
+/// TODO (0.9.1) on first load, multiscale file is created on server, the process is detached. Provide feedback to the user somehow
+
+class MVTimeSeriesView2Private;
+class MVTimeSeriesView2 : public QWidget {
+    Q_OBJECT
+public:
+    friend class MVTimeSeriesView2Private;
+    MVTimeSeriesView2(MVViewAgent* view_agent);
+    virtual ~MVTimeSeriesView2();
+
+    void setSampleRate(double samplerate);
+    void setTimeseries(const DiskReadMda& X);
+    void setMLProxyUrl(const QString& url);
+    void setTimesLabels(const QVector<double>& times, const QVector<int>& labels);
+    void setChannelColors(const QList<QColor>& colors);
+
+    void setTimeRange(MVRange);
+    void setCurrentTimepoint(double t);
+    void setSelectedTimeRange(MVRange range);
+    void setAmplitudeFactor(double factor); // display range will be between -1/factor and 1/factor, but not clipped (thus channel plots may overlap)
+    void autoSetAmplitudeFactor();
+    void autoSetAmplitudeFactorWithinTimeRange();
+    void setActivated(bool val);
+
+    double currentTimepoint() const;
+    MVRange timeRange() const;
+    double amplitudeFactor() const;
+    DiskReadMda timeseries();
+
+    void resizeEvent(QResizeEvent* evt);
+    void paintEvent(QPaintEvent* evt);
+    void mousePressEvent(QMouseEvent* evt);
+    void mouseReleaseEvent(QMouseEvent* evt);
+    void mouseMoveEvent(QMouseEvent* evt);
+    void wheelEvent(QWheelEvent* evt);
+    void keyPressEvent(QKeyEvent* evt);
+
+    static void unit_test();
+
+signals:
+    void clicked();
+
+private
+slots:
+    void slot_scroll_to_current_timepoint();
+
+private:
+    MVTimeSeriesView2Private* d;
+};
+
+#endif // MVTIMESERIESVIEW2_H
