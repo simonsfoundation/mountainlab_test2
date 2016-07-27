8a0edb99e99fbd8437348f049fc08c75688952a8
Jeremy Magland <jeremy.magland@gmail.com>
2016-07-27 18:41:43 -0400

    misc

---
+++ matlab/mountainlab_setup.m
@@ -0,0 +1,15 @@
+function mountainlab_setup
+mfile_path=fileparts(mfilename('fullpath'));
+addpath([mfile_path,'/sorting_algorithms']);
+addpath([mfile_path,'/processing']);
+addpath([mfile_path,'/view']);
+addpath([mfile_path,'/view/colorspace']);
+addpath([mfile_path,'/msutils']);
+addpath([mfile_path,'/3rdparty']);
+addpath([mfile_path,'/3rdparty/jsonlab']);
+addpath([mfile_path,'/unit_tests']);
+addpath([mfile_path,'/validation']);
+addpath([mfile_path,'/misc_examples']);
+addpath([mfile_path,'/../mountainsort/src/isosplit']);
+addpath([mfile_path,'/../spikespy/matlab']); %added on 4/12/16
+end
