7e8babdaadae4acecb409627297c0bd43ca4ef02
Witold Wysota <wysota@wysota.org>
2016-08-02 00:52:52 +0200

    Optimize sorting in mountainsort

--- mountainsort/src/utils/get_sort_indices.cpp
+++ mountainsort/src/utils/get_sort_indices.cpp
@@ -1,54 +1,19 @@
 #include "get_sort_indices.h"
 
 #include <QVector>
-
-struct special_comparer_struct {
-    double val;
-    long index;
-};
-struct special_comparer {
-    bool operator()(const special_comparer_struct& a, const special_comparer_struct& b) const
-    {
-        if (a.val < b.val)
-            return true;
-        else if (a.val == b.val) {
-            return (a.index < b.index);
-        }
-        else
-            return false;
-    }
-};
+#include <algorithm>
 
 QList<long> get_sort_indices(const QList<long>& X)
 {
-    QList<special_comparer_struct> list;
-    for (long i = 0; i < X.count(); i++) {
-        special_comparer_struct tmp;
-        tmp.val = X[i];
-        tmp.index = i;
-        list << tmp;
-    }
-    qSort(list.begin(), list.end(), special_comparer());
-    QList<long> ret;
-    for (int i = 0; i < list.count(); i++) {
-        ret << list[i].index;
-    }
-    return ret;
+    QList<long> result = X;
+    std::stable_sort(result.begin(), result.end());
+    return result;
 }
 
 QList<long> get_sort_indices(const QVector<double>& X)
 {
-    QList<special_comparer_struct> list;
-    for (long i = 0; i < X.count(); i++) {
-        special_comparer_struct tmp;
-        tmp.val = X[i];
-        tmp.index = i;
-        list << tmp;
-    }
-    qSort(list.begin(), list.end(), special_comparer());
-    QList<long> ret;
-    for (int i = 0; i < list.count(); i++) {
-        ret << list[i].index;
-    }
-    return ret;
+    QList<long> result;
+    std::copy(X.constBegin(), X.constEnd(), result.begin());
+    std::stable_sort(result.begin(), result.end());
+    return result;
 }
