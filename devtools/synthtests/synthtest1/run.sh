44f6672ca4056f9db640e094735ab29b08d8cea9
Jeremy Magland <jeremy.magland@gmail.com>
2016-08-08 20:04:11 -0400

    alg_scda_006.js

--- devtools/synthtests/synthtest1/run.sh
+++ devtools/synthtests/synthtest1/run.sh
@@ -2,4 +2,4 @@
 
 mountainprocess run-process synthesize1 --waveforms=waveforms.mda --waveforms_oversamp=10 --info=info.mda --timeseries_out=raw.mda --firings_true=firings_true.mda --N=1e7 --noise_level=0.1 --samplerate=30000
 
-mountainprocess run-script --_nodaemon alg_scda_005.js params.par --raw=raw.mda --geom=geom.csv --outpath=.
+mountainprocess run-script --_nodaemon alg_scda_006.js params2.par --raw=raw.mda --geom=geom.csv --outpath=.
