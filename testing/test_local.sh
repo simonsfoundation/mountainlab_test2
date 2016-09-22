0ee215bb6864f3e78b5e48677488faed042eed2f
Jeremy Magland <jeremy.magland@gmail.com>
2016-09-22 17:04:34 -0400

    moving stuff into WIP folder

--- testing/test_local.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-base1=/home/magland/dev/mountainlab/mdaserver_data
-base2=remote://localhost
-base3=remote://magland
-
-base=$base1
-
-mountainview --mode=overview2 --raw=$base/pre0.mda --filt=$base/pre1b.mda --pre=$base/pre2.mda --firings=$base/firings.mda --samplerate=30000.000000 
