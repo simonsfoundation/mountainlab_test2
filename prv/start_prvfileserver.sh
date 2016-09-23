2336cdb799e70b97509bb3f6f8f03648e0fb6e1a
Jeremy Magland <jeremy.magland@gmail.com>
2016-09-23 18:57:15 +0000

    fixes

--- prv/start_prvfileserver.sh
+++ prv/start_prvfileserver.sh
@@ -4,7 +4,7 @@
 
 # Pass the absolute data directory path in as the first argument.
 
-# Edit the configuration in prv.json. You can start by copying prv.json.default to prv.json.
+# Edit the configuration in prvfileserver.user.json. You can start by copying prvfileserver.default.json to prvfileserver.user.json.
 
 # This script may seem crazy, but it is necessary to do this to achieve that the user
 # inside the container matches exactly the current user (including uid!)
@@ -28,10 +28,10 @@ cmd="nodejs /base/prvfileserver/prvfileserver.js $abs_data_directory"
 
 # We need to map the directory where the data are
 args="--name=prv_container --net=host --pid=host -v $abs_data_directory:$abs_data_directory"
-if [ -f "$PWD/prv.json" ];
+if [ -f "$PWD/prvfileserver.user.json" ];
 then
-  # if it exists, map the prv.json file. Note that the prv.json.file is copied over during build
-  args="$args -v $PWD/prv.json:/base/prv.json"
+  # if it exists, map the prvfileserver.user.json file. Note that the prvfileserver.default.json file is copied over during build
+  args="$args -v $PWD/prvfileserver.user.json:/base/prvfileserver.user.json"
 fi
 
 # Here's the crazy-ish but necessary way to do it
